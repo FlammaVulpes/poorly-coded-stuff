fb05152232fa515dcbffa465addcbe9894d11710
FlammaVulpes <30445877+FlammaVulpes@users.noreply.github.com>
2017-10-02 23:15:56 -0300

    Update knapsack.cpp
    Now this is fixed, for fucks sake.

--- UEL/MD/knapsack.cpp
+++ UEL/MD/knapsack.cpp
@@ -2,10 +2,15 @@
 #include <iomanip>
 #include <cmath>
 #include <algorithm>
+#include <time.h>
+
+// Greedy
 
 using namespace std;
-float capacity = 20;
-float minSize;
+float capacity = 120;
+const int amountOfFiles = 60;
+
+float minSize, diskVal = 0;
 
 struct songs{
     float fileSize;
@@ -15,85 +20,112 @@ struct songs{
 struct fileInfo{
     float valPerUnit;
     bool wasUsed;
+    int fileIndex;
+    float fileSize;
+    float value;
 };
 
-int amountOfFiles = 10;
+songs info[amountOfFiles];
+fileInfo worth[amountOfFiles];
 
-songs info[10];
-fileInfo worth[10];
+int random(int min, int max){ //Range = [min, max]
+   static bool first = true;
+   if(first){
+      srand(time(NULL)); // Seed only once
+      first = false;
+   }
+   return (min + rand() % (max - min));
+}
 
 void setData(){
+    /*
     info[0].fileSize = 3.526;
-    info[0].value = 4;
-    info[1].fileSize = 3.173;
-    info[1].value = 3.6;
-    info[2].fileSize = 4.934;
-    info[2].value = 2;
-    info[3].fileSize = 5.126;
-    info[3].value = 4.3;
-    info[4].fileSize = 7.014;
-    info[4].value = 5.2;
-    info[5].fileSize = 2.927;
-    info[5].value = 3.3;
-    info[6].fileSize = 4.708;
-    info[6].value = 4.2;
-    info[7].fileSize = 3.611;
-    info[7].value = 2.6;
-    info[8].fileSize = 3.147;
-    info[8].value = 3.2;
-    info[9].fileSize = 4.206;
-    info[9].value = 4;
+    info[0].value = 4; */
+    for(int i = 0; i < amountOfFiles; i++){
+        info[i].fileSize = random(2000, 6000);
+        info[i].value = random(1000, 6000);
+        info[i].fileSize /= (float) 1000;
+        info[i].value /= (float) 1000;
+
+        cout << info[i].fileSize << " kB " << "[" << info[i].value << "]";
+        if((i+1) % 3 == 0) cout << endl; else cout << "\t";
+    }
+    cout << endl;
 }
 
 void doWorth(){
     // Calculate all worth before sorting
-    for(int i = 0; i < 10; i++){
+    for(int i = 0; i < amountOfFiles; i++){
         worth[i].valPerUnit = info[i].value/info[i].fileSize;
         // Also, let's make something false right now
         worth[i].wasUsed = false;
+        // Also², let's make sure the program uses the same indexes for both arrays
+        worth[i].fileIndex = i;
+        worth[i].fileSize = info[i].fileSize;
+        worth[i].value = info[i].value;
     }
     // Actually sorting via SELECTION SORT
-    for(int i = 0; i < 9; i++){
+    for(int i = 0; i < amountOfFiles - 1; i++){
         // Assume the first element if the max
         int max = i;
         // Now, loop the entire array to find if there is a higher value
-        for(int j = i + 1; j < 10; j++){
+        for(int j = i + 1; j < amountOfFiles; j++){
             // If it is found, new max index is j
             if(worth[j].valPerUnit > worth[max].valPerUnit) max = j;
         }
         // Swap elements unless the max index is the same as i
-        if(max != i) swap(worth[i].valPerUnit, worth[max].valPerUnit);
+        // Swap indexes as well
+        if(max != i){
+            swap(worth[i].valPerUnit, worth[max].valPerUnit);
+            swap(worth[i].fileIndex, worth[max].fileIndex);
+            swap(worth[i].fileSize, worth[max].fileSize);
+            swap(worth[i].value, worth[max].value);
+        }
+        cout << worth[i].fileSize << " kB " << "[V/U = " << worth[i].valPerUnit << "]";
+        if((i+1) % 3 == 0) cout << endl; else cout << "\t";
     }
 }
 
 void getMinSize(){
-    for(int i = 0; i < 10; i++){
-        if(i == 0) minSize = info[i].fileSize;
+    for(int i = 0; i < amountOfFiles - 1; i++){
+        if(i == 0) minSize = worth[i].fileSize;
         else{
-            if(minSize > info[i].fileSize) minSize = info[i].fileSize;
+            if(minSize > worth[i].fileSize) minSize = worth[i].fileSize;
         }
+    }
+    cout << minSize << endl;
+}
+
+void addToDisk(){
+    int i = 0;
+    while(1){
+        if(!worth[i].wasUsed && (capacity - worth[i].fileSize >= 0)){
+            capacity -= worth[i].fileSize;
+            worth[i].wasUsed = true;
+            diskVal += worth[i].value;
+            cout << "Added song " << i + 1 << " (" << worth[i].fileSize << " kB) [" << worth[i].valPerUnit <<"] to the disk!" << "\tRemaining space: " << capacity << " kB" << endl;
+        }
+        i++;
+        if(capacity < minSize || i >= amountOfFiles) break;
     }
 }
 
 int main(){
+    cout << fixed << setprecision(3);
+    cout << "Running the simulation with " << amountOfFiles << " files and " << capacity << " MB of storage." << endl;
     setData();
     doWorth();
+    cout << endl;
     // Gets the minimum file size
     getMinSize();
 
-    float diskVal = 0, usedPercentage; int i = 0;
-    while(capacity > minSize || i < 10){
-        if(!worth[i].wasUsed && capacity > info[i].fileSize){
-            capacity -= info[i].fileSize;
-            worth[i].wasUsed = true;
-            diskVal += info[i].value;
-            cout << "Added song " << i + 1 << " (" << info[i].fileSize << " kB) to the disk!" << endl;
-            cout << "Remaining space: " << capacity << " kB" << endl;
-        }
-        i++;
-    }
-    usedPercentage = (100/20) * (20 - capacity);
-    cout << "Used percentage: " << usedPercentage << "%" << endl;
+    float usedPercentage, maxCapacity = capacity;
+    cout << endl;
+    // Loop some times (in this case, 4) to guarantee it can fetch the most disks
+    addToDisk();
+    usedPercentage = (100/maxCapacity) * (maxCapacity - capacity);
+    cout << "\nUsed percentage: " << usedPercentage << "%" << endl;
+    cout << "Total value: " << diskVal << endl;
 
     return 0;
 }
