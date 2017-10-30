79d52d0dc6445e8829452664723183dc1318192f
FlammaVulpes <30445877+FlammaVulpes@users.noreply.github.com>
2017-10-30 10:02:10 -0200

    Add files via upload

---
+++ UEL/Lista Strings 1/header.hpp
@@ -0,0 +1,3 @@
+void str5();
+int countSubstring(char*, char*);
+void fetchAndRemove(char*, char*);
---
+++ UEL/Lista Strings 1/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+
+#include "header.hpp"
+
+using namespace std;
+
+int main(){
+    str5();
+    return 0;
+}
