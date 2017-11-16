eb838e1919a13a8ee66be324b55ff41b19a08902
FlammaVulpes <30445877+FlammaVulpes@users.noreply.github.com>
2017-11-16 11:51:15 -0200

    Update Cobrinha.cpp

--- UEL/Cobrinha.cpp
+++ UEL/Cobrinha.cpp
@@ -3,12 +3,8 @@
 #include <cstdlib>
 #include <time.h>
 #include <windows.h>
-/*
-    O que falta fazer:
-        - Renderizar as partes da cobra;
-        - Fazer o negócio pra ir movendo a cobra; OK
-*/
-const int linhas = 15;
+
+const int linhas = 25;
 const int cobraComprimento = 6;
 
 using namespace std;
@@ -23,77 +19,97 @@ Cobra cobra[6];
 
 char c[60][linhas];
 
-void cFill(){
-    for(int y = 0; y < linhas; y++){
-        for(int x = 0; x < 60; x++){
-            if(x == 0 || x == 59 || y == 0 || y == linhas - 1) c[x][y] = 'o'; else c[x][y] = ' ';
-            //cout << c[x][y];
-        }
-        //cout << endl;
-    }
+
+void clearScreen(){
+    system("cls");
 }
 
-void cWrite(){
-    for(int y = 0; y < linhas; y++){
-        for(int x = 0; x < 60; x++){
-            // Falta colocar para renderizar a cobra
-            for(int i = 0; i < 6; i++){
-                if(x == cobra[i].x && y == cobra[i].y){
-                    // Atribuir aqueles negócios lá
-                }
-                cout << c[x][y];
-            }
-        }
-    cout << endl;
-    }
-}
 
-void cobraStart(){
-    x = rand() % 60;
-    y = rand() % 15;
-    for(int i = 0; i < 6; i++){
-        cobra[i].x = x;
-        cobra[i].y = y;
-        cobra[i].t = 6 - i;
-    }
+int doRand(){
+    return (rand() % 3) - 1;
 }
 
 void cobraMove(){
     int dx = 0, dy = 0;
     // Para ver se vai cair no mesmo lugar, se vai cair no lugar anterior ou se vai extrapolar o limite
-    while(dx == 0 && dy == 0 && (cobra[0].x + dx != cobra[1].x && cobra[0].y + du != cobra[1].y) && (cobra[0].x + dx >= 60 || cobra[0].x + dx <= 0 || cobra[0].y + dy >= 15 || cobra[0].y + dy <= 0)){
+    // Só falta arrumar essa parte do negócio sair da borda sem querer
+    // E a porra da cobra se mexer, porque a vadia parou de se mexer do nada (y)
+    while((dx == 0 && dy == 0) && (cobra[0].x + dx == cobra[1].x && cobra[0].y + dy == cobra[1].y)){
         dx = doRand();
         dy = doRand();
+        if(cobra[0].x + dx < 60 && cobra[0].x > 0 && cobra[0].y < linhas - 1 && cobra[0].y > 0) break;
     }
     for(int i = 5; i > 0; i--){
         int tempx = cobra[i - 1].x;
-        int tempy = cobra[i - 1).y;
+        int tempy = cobra[i - 1].y;
         int tempt = cobra[i - 1].t;
 
         cobra[i].x = tempx;
         cobra[i].y = tempy;
         cobra[i].t = tempt - 1;
     }
+    cout << dx << " " << dy << endl;
+
     cobra[0].x += dx;
     cobra[0].y += dy;
     cobra[0].t = 6;
 }
 
-int doRand(){
-    return (rand() % 3) - 1;
+void cFill(){
+    for(int y = 0; y < linhas; y++){
+        for(int x = 0; x < 60; x++){
+            if(x == 0 || x == 59 || y == 0 || y == linhas - 1){
+                c[x][y] = 'o';
+            } else { c[x][y] = ' '; }
+        }
+    }
 }
 
-void clearScreen(){
-    system("cls");
+void cWrite(){
+    cFill();
+    clearScreen();
+    for(int y = 0; y < linhas; y++){
+        for(int x = 0; x < 60; x++){
+            for(int i = 0; i < 6; i++){
+                if(x == cobra[i].x && y == cobra[i].y){
+                    if(cobra[i].t == 1 || cobra[i].t == 2){
+                        c[x][y] = 176;
+                    }
+                    if(cobra[i].t == 3 || cobra[i].t == 4){
+                        c[x][y] = 177;
+                    }
+                    if(cobra[i].t == 5 || cobra[i].t == 6){
+                        c[x][y] = 178;
+                    }
+                }
+            }
+            cout << c[x][y];
+        }
+        cout << endl;
+    }
 }
 
-int checkNext(){
-    // Usar o próximo valor como parâmetro
-    return 0;
+void cobraStart(){
+    int x = 20 + (rand() % 40);
+    int y = 7 + (rand() % (linhas - 7));
+    for(int i = 0; i < 6; i++){
+        cobra[i].x = x - i;
+        cobra[i].y = y;
+        cobra[i].t = 6 - i;
+    }
 }
 
 int main(){
-    cFill();
     srand(time(NULL));
+    int it;
+    int v;
+    cout << "Qual o tempo entre passos, em milissegundos? "; cin >> v;
+    cout << "Quantas iteracoes realizar? "; cin >> it;
+    cobraStart();
+    for(int i = 0; i < it; i++){
+        cWrite();
+        cobraMove();
+        Sleep(v);
+    }
     return 0;
 }
